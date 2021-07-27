be69dacb6d4c5eb484479dd59316bf109ac32809
Daniel Comeaux <58892984+dlcomeaux@users.noreply.github.com>
2021-07-27 16:48:33 -0500

    Create chart

--- script.R
+++ script.R
@@ -11,88 +11,150 @@ source("../mydailytravel/R/helper_fns.R")
 #                 Data Prep                     #
 #                                               #
 #################################################
+# 
+# # Download data (only run once per machine, unless data needs to be updated)
+# divvy_zip <- tempfile()
+# 
+# divvy_urls <- 
+#   c("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202004-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202005-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202006-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202007-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202008-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202009-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202010-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202011-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202012-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202101-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202102-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202103-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202104-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202105-divvy-tripdata.zip",
+#     "https://divvy-tripdata.s3.amazonaws.com/202106-divvy-tripdata.zip"
+#     )
+# 
+# for (i in 1:length(divvy_urls)) {
+#   download.file(divvy_urls[i],divvy_zip,quiet = TRUE)
+#   unzip(divvy_zip,exdir = "data")
+# }
+# 
+# file.remove(divvy_zip)
+# # rm(divvy_zip)
+# 
+# # Extract desired data. Note that the variable names and values changed in 2020
+# # Q1 to reflect e-bikes, remove gender, and change the values of user types
+# 
+# pre_2020_relevant_cols <- c(2,3,10)
+# pre_2020_names <- c("start_time","end_time","usertype")
+# post_2020_relevant_cols <- c(2,3,4,13)
+# 
+# divvy_2019 <-
+#   rbind(read.csv("data/Divvy_Trips_2019_Q1.csv") %>% select(!!pre_2020_relevant_cols),
+#         read.csv("data/Divvy_Trips_2019_Q2.csv") %>% select(!!pre_2020_relevant_cols) %>%
+#           # Fix column names, which are different only for this one for some reason
+#           'colnames<-' (pre_2020_names),
+#         read.csv("data/Divvy_Trips_2019_Q3.csv") %>% select(!!pre_2020_relevant_cols),
+#         read.csv("data/Divvy_Trips_2019_Q4.csv") %>% select(!!pre_2020_relevant_cols)) %>% 
+#   # Add value for bike type
+#   mutate(rideable_type = "docked_bike") %>% 
+#   # Rename column names to match post-2020 values
+#   rename("started_at" = "start_time",
+#          "ended_at" = "end_time",
+#          "member_casual" = "usertype") %>% 
+#   # Recode values from usertype to new system
+#   mutate(member_casual = recode(member_casual,
+#                                 "Subscriber" = "member",
+#                                 "Customer" = "casual"))
+# 
+# write.csv(divvy_2019,"data/divvy2019.csv")
+#   
+# divvy_2020 <-
+#   rbind(read.csv("data/Divvy_Trips_2020_Q1.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202004-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202005-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202006-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202007-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202008-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202009-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202010-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202011-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202012-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols))
+# 
+# write.csv(divvy_2020,"data/divvy2020.csv")
+# 
+# divvy_2021 <-
+#   rbind(read.csv("data/202101-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202102-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202103-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202104-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202105-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
+#         read.csv("data/202106-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols))
+# 
+# write.csv(divvy_2021,"data/divvy2021.csv")
 
-# Download data (only run once per machine, unless data needs to be updated)
-divvy_zip <- tempfile()
+# Load extracted data
 
-divvy_urls <- 
-  c("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202004-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202005-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202006-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202007-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202008-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202009-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202010-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202011-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202012-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202101-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202102-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202103-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202104-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202105-divvy-tripdata.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/202106-divvy-tripdata.zip"
-    )
+divvy_loader <- function(filepath) {
+  read_csv(filepath) %>% 
+    select(-X1) %>%
+    mutate(
+      weekend = case_when(
+        wday(started_at) %in% c(1,7) ~ 1,
+        TRUE ~ 0),
+      month = month(started_at),
+      year = year(started_at),
+      rideable_type = recode(rideable_type,
+                             "classic_bike" = "docked_bike"))
+  }
 
-for (i in 1:length(divvy_urls)) {
-  download.file(divvy_urls[i],divvy_zip,quiet = TRUE)
-  unzip(divvy_zip,exdir = "data")
-}
+divvy_2019 <- divvy_loader("data/divvy2019.csv")
+divvy_2020 <- divvy_loader("data/divvy2020.csv")
+divvy_2021 <- divvy_loader("data/divvy2021.csv")
 
-file.remove(divvy_zip)
-rm(divvy_zip)
+divvy <- rbind(divvy_2019,divvy_2020,divvy_2021)
 
-# Load data (every session unless workspace saved). Note that the variable names
-# and values changed in 2020 Q1 to reflect e-bikes, remove gender, and change
-# the values of user types
-
-pre_2020_relevant_cols <- c(2,3,10)
-pre_2020_names <- c("start_time","end_time","usertype")
-post_2020_relevant_cols <- c(2,3,4,13)
-
-divvy_2019 <-
-  rbind(read.csv("data/Divvy_Trips_2019_Q1.csv") %>% select(pre_2020_relevant_cols),
-        read.csv("data/Divvy_Trips_2019_Q2.csv") %>% select(pre_2020_relevant_cols) %>%
-          # Fix column names, which are different only for this one for some reason
-          'colnames<-' (pre_2020_names),
-        read.csv("data/Divvy_Trips_2019_Q3.csv") %>% select(pre_2020_relevant_cols),
-        read.csv("data/Divvy_Trips_2019_Q4.csv") %>% select(pre_2020_relevant_cols)) %>% 
-  # Add value for bike type
-  mutate(rideable_type = "docked_bike") %>% 
-  # Rename column names to match post-2020 values
-  rename("started_at" = "start_time",
-         "ended_at" = "end_time",
-         "member_casual" = "usertype") %>% 
-  # Recode values from usertype to new system
-  mutate(member_casual = recode(member_casual,
-                                "Subscriber" = "member",
-                                "Customer" = "casual"))
+biketype_p1 <-
+  divvy %>% 
+  group_by(month,year,rideable_type) %>% 
+  summarize(n = n()) %>% 
+  mutate(date = as.Date(paste0(year,"-",month,"-01"))) %>% 
+  mutate(rideable_type = recode_factor(factor(rideable_type),
+                                "docked_bike" = "Standard",
+                                "electric_bike" = "Electric")) %>% 
+  group_by(month,year) %>% 
+  mutate(pct = n/sum(n)) %>% 
   
-divvy_2020 <-
-  rbind(read.csv("data/Divvy_Trips_2020_Q1.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202004-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202005-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202006-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202007-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202008-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202009-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202010-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202011-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202012-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols))
-  
-divvy_2021 <-
-  rbind(read.csv("data/202101-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202102-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202103-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202104-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202105-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols),
-        read.csv("data/202106-divvy-tripdata.csv") %>% select(!!post_2020_relevant_cols))
+  ggplot(aes(x = date,y = n,fill = rideable_type,
+             label = ifelse(rideable_type == "Standard" & date > as.Date("2020-06-30"),
+                            scales::label_percent(accuracy = 1)(pct),""))) +
+  geom_col(position = position_stack(reverse = T)) +
+  theme_cmap(axisticks = "x",xlab = "Total monthly Divvy ridership by bicycle type") + 
+  scale_y_continuous(labels = scales::label_comma(),
+                     n.breaks = 10) +
+  scale_x_date(breaks = seq.Date(as.Date("2019-01-01"),
+                                 as.Date("2021-04-01"),
+                                 by = "3 months"),
+               labels = scales::label_date_short()) +
+  cmap_fill_discrete(palette = "legislation",reverse = T) +
+  geom_text(position = position_stack(vjust = 0.5,reverse = T),
+            color = "white",
+            angle = 90,
+            size = 3
+            )
 
-
+finalize_plot(biketype_p1,
+              "The popularity of electric bikes has contributed to a dramatic
+              increase in Divvy ridership in 2021 vs. prior years.",
+              sidebar_width = 0,
+              filename = "biketype_p1",
+              mode = "png",
+              height = 7.5,
+              overwrite = T)
 
 
 # Create helper values
@@ -102,10 +164,16 @@ threshold_divvy <- as.numeric(ymd_hms("2020-01-01 03:00:00", tz = "America/Chica
 # The number of seconds in a day (used to add days)
 day_value <- 60*60*24
 
+weekday_calculator <- function(start,end) {
+  sum(!wday(seq(as.Date(start),
+                as.Date(end),
+                "days")) %in% c(1,7))
+}
+
 weekdays_2020 <- 
-  sum(!weekdays(seq(as.Date("2020-01-01"),
+  sum(!wday(seq(as.Date("2020-01-01"),
                     as.Date("2020-12-31"),
-                    "days")) %in% c("Saturday","Sunday"))
+                    "days")) %in% c(1,7))
 
 weekdays_2021 <-
   sum(!weekdays(seq(as.Date("2021-01-01"),
